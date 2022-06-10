23162d4820b138c0b4b109a9d3ca744265cd5dd9
OlesiaSub <lesya.sub@mail.ru>
2022-06-10 15:47:59 +0500

    Fix formatting

--- java/debugger/impl/src/com/intellij/debugger/ui/breakpoints/BreakpointManager.java
+++ java/debugger/impl/src/com/intellij/debugger/ui/breakpoints/BreakpointManager.java
@@ -255,6 +255,7 @@ public class BreakpointManager {
   @Nullable
   public MethodBreakpoint addMethodBreakpoint(Document document, int lineIndex) {
     ApplicationManager.getApplication().assertIsDispatchThread();
+
     XLineBreakpoint xBreakpoint = addXLineBreakpoint(JavaMethodBreakpointType.class, document, lineIndex);
     Breakpoint javaBreakpoint = getJavaBreakpoint(xBreakpoint);
     if (javaBreakpoint instanceof MethodBreakpoint) {
